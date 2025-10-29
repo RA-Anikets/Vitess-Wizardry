f052dd62d8929fc25a11185e2d87e544dccfbefb
RA-Anikets <aniket.shelke@rockwellautomation.com>
2025-10-29 15:52:09 +0530

    working out of sync and replica serve issue

--- mysql/scripts/init.sql
+++ mysql/scripts/init.sql
@@ -1,26 +1,3 @@
-CREATE USER 'vt_allprivs'@'%' IDENTIFIED BY 'vitesspass!';
-GRANT ALL PRIVILEGES ON *.* TO 'vt_allprivs'@'%' WITH GRANT OPTION;
-
-CREATE USER 'vt_app'@'%' IDENTIFIED BY 'vitesspass!';
-GRANT SELECT, INSERT, UPDATE, DELETE ON *.* TO 'vt_app'@'%';
-
-CREATE USER 'vt_dba'@'%' IDENTIFIED BY 'vitesspass!';
-GRANT ALL PRIVILEGES ON *.* TO 'vt_dba'@'%' WITH GRANT OPTION;
-
-CREATE USER 'vt_repl'@'%' IDENTIFIED BY 'vitesspass!';
-GRANT REPLICATION SLAVE ON *.* TO 'vt_repl'@'%';
-
-CREATE USER 'vt_super'@'%' IDENTIFIED BY 'vitesspass!';
-GRANT SUPER, PROCESS, RELOAD, SHUTDOWN ON *.* TO 'vt_super'@'%';
-
-CREATE USER 'vt_filtered'@'%' IDENTIFIED BY 'vitesspass!';
-GRANT SELECT ON *.* TO 'vt_filtered'@'%';
-
-
-CREATE DATABASE IF NOT EXISTS cmms;
-
-FLUSH PRIVILEGES;
-
 -- load plugins
 INSTALL PLUGIN rpl_semi_sync_master SONAME 'semisync_master.so';
 INSTALL PLUGIN rpl_semi_sync_slave SONAME 'semisync_slave.so';
@@ -32,4 +9,3 @@ SET GLOBAL rpl_semi_sync_slave_enabled = ON;
 -- check status
 SHOW VARIABLES LIKE 'rpl_semi_sync%';
 SHOW PLUGINS;
-
